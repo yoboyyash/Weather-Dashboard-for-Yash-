6de0cffcc3a3a8d64b4a2d2a1406a13e35c7b16b
yoboyyash <yashrajbaviskar@gmail.com>
2024-01-07 03:16:56 -0600

    my assignment got graded mid coding friday night, but I was late

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ async function checkWeather(city) {
 
     console.log(data);
 
-   
+    
     // The following inserts coordinates of the city searched from the data fetched from above api 
     let APIURL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${API_Key}`;
 
@@ -55,7 +55,9 @@ async function checkWeather(city) {
         document.querySelector("#temp1").innerHTML = "Temperature: " + Math.round(((fiveDaysForecast[1].main.temp - 273.15).toFixed(2)*(9/5)+32)) + " °F";
         document.querySelector("#wind1").innerHTML = "Wind: " + fiveDaysForecast[1].wind.speed + " Mhp";
         document.querySelector("#humidity1").innerHTML = fiveDaysForecast[1].main.humidity + "%";
-       
+        
+    
+
         document.querySelector("#temp2").innerHTML = "Temperature: " + Math.round(((fiveDaysForecast[2].main.temp - 273.15).toFixed(2)*(9/5)+32)) + " °F";
         document.querySelector("#wind2").innerHTML = "Wind: " + fiveDaysForecast[2].wind.speed + " Mhp";
         document.querySelector("#humidity2").innerHTML = fiveDaysForecast[2].main.humidity + "%";
@@ -71,15 +73,17 @@ async function checkWeather(city) {
         document.querySelector("#temp5").innerHTML = "Temperature: " + Math.round(((fiveDaysForecast[5].main.temp - 273.15).toFixed(2)*(9/5)+32)) + " °F";
         document.querySelector("#wind5").innerHTML = "Wind: " + fiveDaysForecast[5].wind.speed + " Mhp";
         document.querySelector("#humidity5").innerHTML = fiveDaysForecast[5].main.humidity + "%";
-                 
+        
+      
 
+       
     }
 
     getWeatherDetails(); // Starts function getWeatherDetails but only after the above checkWeather starts via eventlistener
 
     // Inserts data into html from first api data pull (this only makes the current forecast)
-    document.querySelector("#city-name").innerHTML = "City Name: " + data.name;
-    document.querySelector("#temp").innerHTML = "Temperature: " + data.main.temp + " °F";
+    document.querySelector("#city-name").innerHTML = data.name;
+    document.querySelector("#temp").innerHTML = "Temperature: " + Math.round(data.main.temp) + " °F";
     document.querySelector("#wind").innerHTML = "Wind: " + data.wind.speed + " Mhp";
     document.querySelector("#humidity").innerHTML = data.main.humidity + "%";
     
@@ -96,6 +100,60 @@ async function checkWeather(city) {
     // Inserts loaded icon into html to show condition image
     let h1 = document.querySelector("#weather-icon");
     h1.appendChild(loadIcon);
+    
+    async function fiveDayIcons() {
+        let response = await fetch(APIURL2);
+        let data2 = await response.json(); 
+        let forecastDateFilter = [];
+
+
+        let fiveDaysForecast = data2.list.filter(fforecast => {
+            let fforecastDate = new Date(fforecast.dt_txt).getDate();
+            if(!forecastDateFilter.includes(fforecastDate)) {
+                return forecastDateFilter.push(fforecastDate)
+            }
+        });
+        
+
+        let weatherIcon1 = fiveDaysForecast[1].weather[0].icon;
+        let loadIcon1 = document.createElement("img");
+        loadIcon1.setAttribute("src", "https://openweathermap.org/img/w/" + weatherIcon1 + ".png");
+    
+        let icon1 = document.querySelector("#weather-icon1");
+        icon1.appendChild(loadIcon1);
+
+        console.log(loadIcon1);
+
+        let weatherIcon2 = fiveDaysForecast[2].weather[0].icon;
+        let loadIcon2 = document.createElement("img");
+        loadIcon2.setAttribute("src", "https://openweathermap.org/img/w/" + weatherIcon2 + ".png");
+
+        let icon2 = document.querySelector("#weather-icon2");
+        icon2.appendChild(loadIcon2);
+
+        let weatherIcon3 = fiveDaysForecast[3].weather[0].icon;
+        let loadIcon3 = document.createElement("img");
+        loadIcon3.setAttribute("src", "https://openweathermap.org/img/w/" + weatherIcon3 + ".png");
+
+        let icon3 = document.querySelector("#weather-icon3");
+        icon3.appendChild(loadIcon3);
+
+        let weatherIcon4 = fiveDaysForecast[4].weather[0].icon;
+        let loadIcon4 = document.createElement("img");
+        loadIcon4.setAttribute("src", "https://openweathermap.org/img/w/" + weatherIcon4 + ".png");
+
+        let icon4 = document.querySelector("#weather-icon4");
+        icon4.appendChild(loadIcon4);
+
+        let weatherIcon5 = fiveDaysForecast[5].weather[0].icon;
+        let loadIcon5 = document.createElement("img");
+        loadIcon5.setAttribute("src", "https://openweathermap.org/img/w/" + weatherIcon5 + ".png");
+
+        let icon5 = document.querySelector("#weather-icon5");
+        icon5.appendChild(loadIcon5);
+
+    }
+    fiveDayIcons();
     
 
 
